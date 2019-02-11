03ce935a600146586d66a3de1df821ce84f77a09
Fabian le Maux <fabian.lemaux@hotmail.fr>
2019-02-11 10:42:37 +0100

    asd

--- bot.js
+++ bot.js
@@ -143,15 +143,3 @@ async function generate_training(message) {
 
 bot.login(process.env.TOKEN);
 
-//Keeping the bot alive
-const http = require('http');
-const express = require('express');
-const app = express();
-app.get("/", (request, response) => {
-    console.log(Date.now() + " Ping Received");
-response.sendStatus(200);
-});
-app.listen(process.env.PORT);
-setInterval(() => {
-    http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
-}, 280000);
