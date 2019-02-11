9ead6774790665e9c19724b64724a65a1c3b5ee9
Fabian le Maux <fabian.lemaux@hotmail.fr>
2019-02-11 09:19:13 +0100

    Huge advancement

--- bot.js
+++ bot.js
@@ -1,7 +1,8 @@
-var Discord = require('discord.io');
+const Discord = require("discord.js");
 var logger = require('winston');
 var auth = require('./auth.json');
-var botPrefix = '!ztr';
+var botPrefix = auth.prefix;
+
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console, {
@@ -9,66 +10,130 @@ logger.add(new logger.transports.Console, {
 });
 logger.level = 'debug';
 // Initialize Discord Bot
-var bot = new Discord.Client({
+const bot = new Discord.Client({
     token: auth.token,
     autorun: true
 });
-bot.on('ready', function (evt) {
+
+bot.on('ready', () => {
     logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
-    bot.game = '!ztr';
-});
+logger.info('Logged in as: ');
+logger.info(bot.username + ' - (' + bot.id + ')');
+bot.user.setActivity("!ztr");
+})
+;
 
 // Basic Answers
-function foo(bot, user, userID, channelID, message, evt) {
-    bot.sendMessage({
-        to: channelID,
-        message: '**Hi**\n' +
+function whoami(message) {
+    message.channel.send(
+        '**Hi**\n' +
         'Nice to meet you ! My name is **Zoulou** and i am a **brawlhalla personnal trainer**.\n' +
         'I am able to build you a training plan based on your preference for you to **get better** at this game ;)\n' +
-        'Call me : \"**' + botPrefix + ' trainme**\" to begin the training <3 or use \"**' + botPrefix + '** help\" to get any information :)'
-    });
+        'Call me : \"**' + botPrefix + ' trainme**\" to begin the training <3 or use \"**' + botPrefix + ' help**\" to get any information :)'
+    );
 }
 
-function ans_help(bot, user, userID, channelID, message, evt) {
-    bot.sendMessage({
-        to: channelID,
-        message: 'Oh you got trouble using me ? Let me explain.\n' +
-            'First of all the prefix to invok me is !ztr\n' +
-            'Here is the command list that you can use :\n\n' +
-            '======================================\n\n' +
-            ' * **!ztr whoami**   \t\t\t\tA quick presentation of me, Zoulou\n' +
-            ' * **!ztr help**       \t\t\t\tOpen this menu\n' +
-            ' * **!ztr traime**   \t\t\t\tIll slide in your dm to build you a personnal training program <3\n'
-    });
+function ans_help(message) {
+
+    message.channel.send('Oh you got trouble using me ? Let me explain.\n' +
+        'First of all, the prefix to invok me is !ztr\n' +
+        'Here is the command list that you can use :\n\n' +
+        '======================================\n\n' +
+        ' * **!ztr whoami:    A quick presentation of me, Zoulou**\n' +
+        ' * **!ztr help:      Open this menu**\n' +
+        ' * **!ztr trainme:   Ill slide in your dm to build you a personnal training program <3**\n'
+    );
 }
 
-bot.on('message', function (user, userID, channelID, message, evt) {
-    if (message.substring(0, botPrefix.length) == botPrefix) {
-        var args = message.substring(botPrefix.length).split(' ');
-        var cmd = args[1];
+bot.on('message', async message => {
+    if (message.author.bot || message.content.indexOf(botPrefix) !== 0) return;
+    var args = message.content.slice(auth.prefix.length).trim().split(/ +/g);
+    var cmd = args.shift().toLowerCase();
 
-        if (args[1] == null) {
-            ans_help(bot, user, userID, channelID, message, evt);
-            return;
-        }
-        else {
-            bot.sendMessage({
-                to: channelID,
-                message: 'je suis pas vide :[' + args[1] + ']'
-            });
-        }
-        switch (cmd) {
-            // !ping
-            case 'whoami':
-                foo(bot, user, userID, channelID, message, evt);
-                break;
-            case 'help':
-            default:
-                ans_help(bot, user, userID, channelID, message, evt);
-                break;
-            // Just add any case commands if you want to..
-        }
+    if (cmd.length == 0 || cmd == null) {
+        whoami(message);
+        return;
+    }
+
+    switch (cmd) {
+        case 'whoami':
+            whoami(message);
+            break;
+        case 'trainme':
+            generate_training(message);
+            break;
+        case 'help':
+            ans_help(message)
+        default:
+            whoami(message);
+            break;
+        // Just add any case commands if you want to..
     }
 });
+
+
+//===================
+
+
+async function gen_msg(message, weaponSet, idx, size) {
+    weaponSet.msgslot = await message.author.send(weaponSet.name);
+    if (idx > 0) {
+        weaponSet.msgslot.react('⬆');
+    }
+    if (idx < size - 1) {
+        weaponSet.msgslot.react('⬇');
+    }
+    if (idx > 1) {
+        weaponSet.msgslot.react('⏫');
+    }
+
+    if (idx < size - 2) {
+        weaponSet.msgslot.react('⏬');
+    }
+}
+
+async function generate_training(message) {
+    if (message.channel != message.author.dmChannel) {
+        message.channel.send(`Seems like you requested a training ${message.author} check your dm's ma boy 👌`);
+    }
+    var m = await message.author.send("Allright let's build this training plan.\n" +
+        "I might need a little help to know your weapon preferences ;)\n" +
+        "You can sort the list below in order to get the prefered weapon training consequently\n" +
+        "**(this might take a little while to generate the choices so be patients <3)**\n" +
+        "=======================" +
+        "**validate here when it's done**\n");
+    m.react('☑');
+    m = null;
+
+    var tmp = {
+        name: null,
+        msgslot : null
+    };
+    var weaponOrder = new Map();
+
+    weaponOrder.set("Sword", null);
+    weaponOrder.set("Cannon", null);
+    weaponOrder.set("Katars", null);
+    weaponOrder.set("Cannon", null);
+    weaponOrder.set("Scythe", null);
+    weaponOrder.set("Rocket Lance", null);
+    weaponOrder.set("Spear", null);
+    weaponOrder.set("Gauntlet", null);
+    weaponOrder.set("Axe", null);
+    weaponOrder.set("Bow", null);
+    weaponOrder.set("Orb", null);
+    weaponOrder.set("Hammer", null);
+    weaponOrder.set("Blasters", null);
+
+
+
+    var i = 0;
+    for (var [weapName, msg] of weaponOrder) {
+        tmp.name = weapName;
+        tmp.msgslot = null;
+        await gen_msg(message, tmp, i++, weaponOrder.size);
+        msg = tmp.msgslot;
+    }
+}
+
+bot.login(auth.token);
